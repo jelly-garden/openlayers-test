62aac7f014e94b04d5a0a2e8442add6e27facf5a
장정원 <longthegarden@innodep.com>
2023-05-03 16:13:54 +0900

    feat: Layer 만들기

--- main.js
+++ main.js
@@ -3,24 +3,15 @@ import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
 
-// // 기본
-// const source = new OSM();
-// 옵션 적용
 const source = new OSM({
   attributions:
     '<p>Developed by <a href="https://itcode.dev" target="_blank">RWB</a></p>',
   cacheSize: 0,
 });
 
-const map = new Map({
-  target: "map",
-  layers: [
-    new TileLayer({
-      source: source,
-    }),
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2,
-  }),
+const layer = new TileLayer({
+  source: source,
+  properties: { name: "base-osm" },
+  zIndex: 1,
+  preload: Infinity,
 });
