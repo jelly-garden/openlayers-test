3db90adaf67787c2ac084ae60051eaa909d7385f
장정원 <longthegarden@innodep.com>
2023-05-03 15:46:38 +0900

    feat: OSM Source 만들기

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import "./style.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+
+// // 기본
+// const source = new OSM();
+// 옵션 적용
+const source = new OSM({
+  attributions:
+    '<p>Developed by <a href="https://itcode.dev" target="_blank">RWB</a></p>',
+  cacheSize: 0,
+});
 
 const map = new Map({
-  target: 'map',
+  target: "map",
   layers: [
     new TileLayer({
-      source: new OSM()
-    })
+      source: source,
+    }),
   ],
   view: new View({
     center: [0, 0],
-    zoom: 2
-  })
+    zoom: 2,
+  }),
 });
